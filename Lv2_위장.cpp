ba5814ae6acc143f19151f076212ab9f7ffc07f1
zpfhfh0124 <zpfhfh0124@naver.com>
2023-01-20 16:15:00 +0900

    Lv2 튜플
    풀이 완료

---
+++ Lv2_위장.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1; 
+    
+    map<string, int> map_clothes;
+
+    for (auto it = clothes.begin(); it != clothes.end(); it++) {
+        map_clothes[(*it)[1]]++;
+    }
+
+    // n차 다항식 ( n = 종류 개수 ) 의 계수들의 합
+    for (auto it = map_clothes.begin(); it != map_clothes.end(); it++) {
+        answer *= (*it).second + 1;
+    }
+
+    return answer-1;
+}
+
+int main()
+{
+    // value, key
+    vector<vector<string>> clothes = { {"yellow_hat", "headgear"},
+                                       {"blue_sunglasses", "eyewear"},
+                                       {"green_turban", "headgear"} };
+
+    int result = solution(clothes);
+    cout << result << endl;
+}
---
+++ Lv2_튜플.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+typedef pair<int, int> p_ii;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    map<int, int> map_tuple;
+    int digit_cnt = 0;
+    int num = 0;
+    for (auto c = s.end() - 1; c != s.begin(); c--)
+    {
+        if (isdigit(*c) != 0) {
+            num += (*c - '0')* pow(10, digit_cnt);
+            digit_cnt++;
+        }
+        else {
+            if (digit_cnt > 0) {
+                map_tuple[num]++;
+                num = 0;
+                digit_cnt = 0;
+            }
+        }
+    }
+
+    vector<p_ii> vec_tuple(map_tuple.begin(), map_tuple.end());
+    
+    sort(vec_tuple.begin(), vec_tuple.end(), [](p_ii a, p_ii b) {
+        return a.second > b.second;
+    });
+
+    for (auto t : vec_tuple) {
+        answer.push_back(t.first);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    string s = "{{20,111},{111}}";
+    solution(s);
+}
