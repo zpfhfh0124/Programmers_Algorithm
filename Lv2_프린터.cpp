53a3ec869779ebaf3112595c5fe2ac0f12502b5a
zpfhfh0124 <zpfhfh0124@naver.com>
2023-02-07 15:17:44 +0900

    Update Lv2_프린터.cpp

--- Lv2_프린터.cpp
+++ Lv2_프린터.cpp
@@ -1,21 +1,44 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
+    queue<pair<int, int>> task; // index, priority
+    priority_queue<int> pq;
 
-    int max_p = *max_element(priorities.begin(), priorities.end());
-
-    queue<int> q_priorities;
-    for (auto p : priorities) {
-        q_priorities.push(p);
+    for (int i = 0; i < priorities.size(); i++) {
+        task.push(make_pair(i, priorities[i]));
+        pq.push(priorities[i]);
     }
 
-
+    while (!task.empty())
+    {
+        auto cur_task = task.front();
+        task.pop();
+        if (pq.top() == cur_task.second)
+        {
+            pq.pop();
+            answer++;
+            if (cur_task.first == location) break;
+        }
+        else 
+        {
+            task.push(cur_task);
+        }
+    }
 
     return answer;
+}
+
+int main()
+{
+    vector<int> priorities = { 2,1,3,2 };
+    int location = 2;
+
+    int result = solution(priorities, location);
+    cout << result << endl;
 }
