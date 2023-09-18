89c6c48e41e615dfaaf04476243ecad03519943a
zpfhfh0124 <zpfhfh0124@naver.com>
2023-09-18 15:48:38 +0900

    Create BOJ_2738_행렬덧셈.cpp

---
+++ BOJ/BOJ_2738_행렬덧셈.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int sum_mat[100][100]{};
+    int k;
+
+    for (int i = 0; i < n * 2; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> k;
+            sum_mat[(i % n)][j] += k;
+            if (i >= n)cout << sum_mat[i % n][j] << " ";
+        }
+        if (i >= n)cout << endl;
+    }
+
+    return 0;
+}
